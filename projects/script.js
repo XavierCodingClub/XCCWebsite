7e89e011619cdd32836ffd164e0c0271171e6245
z4ab <zabdulam33r@gmail.com>
2021-02-09 20:19:59 -0500

    Add links
    and test "project"

--- projects/script.js
+++ projects/script.js
@@ -2,15 +2,27 @@ var projects = [
     {
         "name": "XCCWebsite",
         "description": "The website we use to promote the club (you're looking at it right now!)",
+        "links": {
+            "github": "https://github.com/XavierCodingClub/XCCWebsite",
+        },
     },
     {
-        "name": "CCC Prep",
-        "description": "Plan to prepare club members for the upcoming 2021 CCC",
-    },
-    {
-        "name": "Meet Your Maker",
-        "description": "A 2D survival game where you- blah blah blah the project is kinda dead tho"
+        "name": "vscode",
+        "description": "ever heard of it?",
+        "links": {
+            "github": "https://github.com/microsoft/vscode",
+        },
     }
+    /*
+    {
+        "name": "ccc prep",
+        "description": "plan to prepare club members for the upcoming 2021 ccc",
+    },
+    {
+        "name": "meet your maker",
+        "description": "a 2d survival game where you- blah blah blah the project is kinda dead tho"
+    }
+    */
 ]
 var container = document.getElementById("container")
 var pTemplate = document.getElementById("ptemplate")
@@ -18,6 +30,7 @@ for (let i = 0; i < projects.length; i++) {
     const element = projects[i];
     var clone = pTemplate.cloneNode(true)
     clone.id=""
+    clone.querySelector("#github").onclick = () => window.open(element["links"]["github"])
     clone.querySelector("h2").textContent = element["name"]
     clone.querySelector("p").textContent = element["description"]
     container.append(clone)
