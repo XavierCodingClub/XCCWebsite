1201f48605ad39c3de8571f34d2a7cc43a68d3e7
Yo hello <63665090+Yohello1@users.noreply.github.com>
2021-05-24 19:21:47 -0400

    Made this cleaner

--- contact/index.php
+++ contact/index.php
@@ -1,10 +1,7 @@
 <head>
   <title>Xavier Coding Club</title>
   <link rel="preconnect" href="https://fonts.gstatic.com" />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap"
-    rel="stylesheet"
-  />
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet"/>
   <link href="/index.css" rel="stylesheet" type="text/css" />
   <link rel="icon" href="images/elogo_1.png">
 </head>
