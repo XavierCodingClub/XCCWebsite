beaf5b1694e5921949be9bd2f17dea537750af3d
Yohello1 <mp2702737@gmail.com>
2021-09-23 23:42:40 -0400

    Starting work on the exec page, dont tell luke the credits page will take a while

--- execs.php
+++ execs.php
@@ -1,28 +1,27 @@
 <head>
   <title>Xavier Coding Club</title>
   <link rel="preconnect" href="https://fonts.gstatic.com" />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap"
-    rel="stylesheet"
-  />
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet"/>
   <link href="/index.css" rel="stylesheet" type="text/css" />
   <link rel="icon" href="/images/elogo_1.png">
 </head>
+<!-- This reminds me I gotta rework the css stuff, it might happen at some point in th future but 
+ehh who cares anyways. No one besides me is gonna look at this heck hole -->
+<style>
+.section_potrait{
+    
+}
 
+</style>
 <body>
   <script src="/utils.js"></script>
-  <?php
-    $year = date("Y");
-    $month = date("m");
-  ?>
+
 	<?php
-  $menu = fopen("menu.html", "r") or die("ERROR! UNABLE TO OPEN MENU FILE");
-  echo fread($menu, filesize("menu.html"));
-  fclose($menu);
+require 'menu.php';
   ?>
   <div class="content">
-    <h1>Welcome to XCC</h1>
-
-
+    <h1>Execs Of XCC</h1>
+<!-- how do I do math again -->
+asdasds
   </div>
 </body>
